c808dc9d351c5c9e0ffd25dd3d7d63ed149aedab
Yi Wen <yiwen@Dragonite.local>
2023-03-06 15:34:30 -0800

    Add init.lua

---
+++ init.lua
@@ -0,0 +1,65 @@
+-- Tab set to two spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use 'joshdick/onedark.vim'
+ 
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+end)
+
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+pcall(vim.cmd, 'colorscheme onedark')
+
+vim.api.nvim_set_keymap(
+  "n",
+  ";",
+  ":",
+  { noremap = true }
+ )
+
+vim.api.nvim_set_keymap(
+  "n",
+  "fb",
+  ":b#<CR>",
+  { noremap = true }
+ )
+
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+lsp.nvim_workspace()
+lsp.setup()
